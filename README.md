c9b308f9f607091d158abd9e9a8b3ae0f43f59a6
Baju Judiantara <judiantara@gmail.com>
2025-03-22 16:48:00 +0700

    Wyvern easy install mark I

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nix flake for easy and secure NixOS installation
+
+Installing NixOS on LUKS with yubikey pre-boot authentication should as easy as
+
+```
+install-nixos
+```
---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Wyvern the NixOS installer";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+      #url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+  };
+
+  outputs = {self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    
+    pbkdf2-sha512 = pkgs.callPackage ./pbkdf2-sha512 { };
+    
+    rbtohex = pkgs.writeShellScriptBin
+      "rbtohex"
+     (builtins.readFile ./scripts/rbtohex.sh);
+
+    hextorb = pkgs.writeShellScriptBin
+      "hextorb"
+     (builtins.readFile ./scripts/hextorb.sh);
+
+    yk-luks-gen = pkgs.writeShellScriptBin 
+     "yk-luks-gen" 
+     (builtins.readFile ./scripts/yk-luks-gen.sh);
+
+    run-installer = pkgs.writeShellScriptBin 
+     "run-installer" 
+     (builtins.readFile ./scripts/run-installer.sh);
+
+    run-ssh-patcher = pkgs.writeShellScriptBin 
+     "run-ssh-patcher" 
+     (builtins.readFile ./scripts/run-ssh-patcher.sh);
+
+  in {
+    devShells.${system} = {
+      run-ssh-patcher = pkgs.mkShell {
+        packages = with pkgs; [
+          rage
+          run-ssh-patcher
+        ];
+        shellHook = ''
+          exec run-ssh-patcher
+        '';
+      };
+  
+      run-installer = pkgs.mkShell {
+        packages = with pkgs; [
+          rage
+          disko
+          cryptsetup
+          openssl
+          parted
+          yubikey-personalization
+          tera-cli
+          pbkdf2-sha512
+          rbtohex
+          hextorb
+          yk-luks-gen
+          run-installer
+        ];
+        shellHook = ''
+          exec run-installer
+        '';
+      };
+    };
+  };
+}
